5af162e1d0f3c5f120f5d234fd0da38b47313e51
crist <c.gallegos09@ufromail.cl>
2025-03-31 23:39:13 -0300

    Creo clase de medico con algunos getters y setters

---
+++ src/main/java/org/example/Medico.java
@@ -0,0 +1,31 @@
+package org.example;
+
+public class Medico {
+    private String nombre;
+    private String especialidad;
+    private boolean disponible;
+
+    public Medico(String nombre, String especialidad, boolean disponible) {
+        this.nombre = nombre;
+        this.especialidad = especialidad;
+        this.disponible = disponible;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getEspecialidad() {
+        return especialidad;
+    }
+
+    public boolean isDisponible() {
+        return disponible;
+    }
+
+    public void setDisponible(boolean disponible) {
+        this.disponible = disponible;
+    }
+
+
+}
