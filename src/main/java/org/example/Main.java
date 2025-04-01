bdc7cd3192cd3da238c952e9d38a9f2550ae5f67
crist <c.gallegos09@ufromail.cl>
2025-04-01 00:59:28 -0300

    Llenamos el programa main donde probamos el funcionamiento

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,31 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        // centro clinico
+        CentroClinico centro = new CentroClinico("Clínica Salud", "Calle123");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        // Creamos medicos
+        Medico medico1 = new Medico("Dr.", "Cardiología", true);
+        Medico medico2 = new Medico("Dra. ", "Pediatría", true);
+        centro.agregarMedico(medico1);
+        centro.agregarMedico(medico2);
+
+        // Creamos pacientes
+        Paciente paciente1 = new Paciente("Juan", 30, "987654321");
+        Paciente paciente2 = new Paciente("Ana ", 25, "123456789");
+
+        // Crear gestor de citas
+        GestorCitas gestor = new GestorCitas();
+
+        // Agendar una cita
+        CitaMedica cita1 = new CitaMedica(medico1, paciente1, "10/07/2024", "15:00", "Revisión general");
+        gestor.agendarCita(cita1);
+
+        // Mostrar citas agendadas
+        gestor.listarCitas();
+
+        // Cancelar la cita
+        gestor.cancelarCita(cita1);
     }
 }
